4efc99c80072937066a5bd9627abdaaac12969f8
unknown <dasarathan.sampath@gmail.com>
2022-08-11 12:08:17 +0530

    oops2 added

--- oops/main.cpp
+++ oops/main.cpp
@@ -1,32 +1,52 @@
-#include<iostream>
-#include<string>
-#include<vector>
+#include <iostream>
+#include <string>
 
-using namespace std;
+#include "Movies.h"
 
-class Player{
-    //attributes
-    string name;
-    int health;
-    int xp;
-    //methods
-    void talk(string);
-    bool is_dead();
-};
+void add_movie(Movies &movies, std::string name, std::string rating, int watched);
 
-int main() {
-    Player dasa;
-    Player kamalini;
-    Player pranava;
+void add_movie(Movies &movies, std::string name, std::string rating, int watched){
+    if(movies.add_movie(name, rating, watched)){
+        std::cout << name << " added" << std::endl;
+    } else {
+        std::cout << name << " already exists" << std::endl;
+    }
+}
 
-    Player players[]{kamalini, pranava};
+int main(){
+    Movies my_movies;
+    my_movies.display();
 
-    vector<Player> player_vec{dasa};
-    player_vec.push_back(kamalini);
+    add_movie(my_movies, "Bigil", "PG-13", 12);
+    add_movie(my_movies, "Beast", "U/A", 7);
 
-    Player *enemy(nullptr);
-    enemy = new Player;
-    delete enemy;
+    my_movies.display();
+}
+
+/* #include "Movies.h"
+
+void add_movies(Movies &movies, std::string name, std::string rating, int watched);
+
+
+void add_movies(Movies &movies, std::string name, std::string rating, int watched){
+    if(movies.add_movies(name, rating, watched)){
+        std::cout << name << " added" << std::endl;
+    } else {
+        std::cout << name << " already exists" << std::endl;
+    }
+} */
+
+/* int main() {
+
+    Movies my_movies;
+
+    my_movies.display();
+
+    add_movies(my_movies, "Bigil", "PG-13", 20);
+    add_movies(my_movies, "Vikram", "PG", 7);
+    add_movies(my_movies, "Ponniyin Selvan", "U/A", 5);
+
+    my_movies.display();
 
     return 0;
-}
+} */
